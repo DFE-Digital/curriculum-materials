e34450e97d1e409305ccb43212f013a439fd3fa1
Peter Yates <peter.yates@graphia.co.uk>
2020-01-28 16:36:57 +0000

    Appease the linter

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -5,8 +5,8 @@ module ApplicationHelper
     end
   end
 
-  def govuk_chevron(colour="currentColor")
-    content_tag :svg, class:"govuk-button__start-icon", xmlns: "http://www.w3.org/2000/svg", width: "17.5", height: "19", viewBox: "0 0 33 40", role: "presentation", focusable: "false" do
+  def govuk_chevron(colour = "currentColor")
+    content_tag :svg, class: "govuk-button__start-icon", xmlns: "http://www.w3.org/2000/svg", width: "17.5", height: "19", viewBox: "0 0 33 40", role: "presentation", focusable: "false" do
       tag.path fill: colour, d: "M0 0h13l20 20-20 20H0l20-20z"
     end
   end
