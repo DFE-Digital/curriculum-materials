ca8bf160391db5a9bfdd475a37456f61b7a60e1a
Peter Yates <peter.yates@graphia.co.uk>
2020-01-29 16:26:21 +0000

    Add the lessons controller and show action

--- db/seeds.rb
+++ db/seeds.rb
@@ -19,7 +19,40 @@ unless Rails.env.test?
           overview: 'Geologists generally group volcanoes into four main kinds; cinder cones, composite volcanoes, shield volcanoes, and lava domes.'
         },
         lessons: [
-          { name: 'Fissure vents' },
+          {
+            name: 'Fissure vents',
+            misconceptions: [
+              "The fragments that form a spatter cone are cool and aren't hot enough to weld together",
+              "Only isolated lava fountains along the fissure produce crater rows of small spatter and cinder cones."
+            ],
+            core_knowledge: <<~CORE_KNOWLEDGE,
+              In Iceland, volcanic vents, which can be long fissures, often
+              open parallel to the rift zones where the Eurasian and the North
+              American Plate lithospheric plates are diverging, a system which
+              is part of the Mid-Atlantic Ridge. Renewed eruptions generally
+              occur from new parallel fractures offset by a few hundred to
+              thousands of metres from the earlier fissures. This distribution
+              of vents and sometimes voluminous eruptions of fluid basaltic
+              lava usually builds up a thick lava plateau, rather than a single
+              volcanic edifice. But there are also the central volcanoes,
+              composite volcanoes, often with calderas, which have been formed
+              during thousands of years, and eruptions with one or more magma
+              reservoirs underneath controlling their respective fissure system
+            CORE_KNOWLEDGE
+
+            summary: <<~SUMMARY
+              A fissure vent, also known as a volcanic fissure, eruption
+              fissure or simply a fissure, is a linear volcanic vent through
+              which lava erupts, usually without any explosive activity. The
+              vent is often a few metres wide and may be many kilometres long.
+              Fissure vents can cause large flood basalts which run first in
+              lava channels and later in lava tubes. After some time the
+              eruption builds up spatter cones and may concentrate on one or
+              some of them. Small fissure vents may not be easily discernible
+              from the air, but the crater rows or the canyons built up by some
+              of them are.
+            SUMMARY
+          },
           { name: 'Shield volcanoes' },
           { name: 'Lava domes' },
           { name: 'Cryptodomes' },
