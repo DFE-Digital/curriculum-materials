73c3b8fc6ed3d36158a52833d08d4e8642402ca2
Peter Yates <peter.yates@graphia.co.uk>
2020-02-04 12:06:01 +0000

    Add ensure_token_exists action to base controller

--- app/controllers/teachers/base_controller.rb
+++ app/controllers/teachers/base_controller.rb
@@ -1,4 +1,13 @@
 module Teachers
   class BaseController < ApplicationController
+    before_action :ensure_token_exists
+
+  private
+
+    def ensure_token_exists
+      unless Teacher.exists?(token: session[:token])
+        render(json: { authorized: false }, status: :unauthorized)
+      end
+    end
   end
 end
