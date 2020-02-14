1757ef04e911b077a98541263420a8960184eb7f
Peter Yates <peter.yates@graphia.co.uk>
2020-02-14 09:41:04 +0000

    Add Activity API create action

--- app/controllers/api/v1/activities_controller.rb
+++ app/controllers/api/v1/activities_controller.rb
@@ -22,12 +22,31 @@ class Api::V1::ActivitiesController < Api::BaseController
   end
 
   def create
+    lesson_part = LessonPart.find_by!(lesson_id: params[:lesson_id], id: params[:lesson_part_id])
+    activity = lesson_part.activities.new(activity_params)
+
+    if activity.save
+      render(json: serialize(activity).to_json, status: :created)
+    else
+      render(json: { errors: activity.errors.full_messages }, status: :bad_request)
+    end
   end
 
   def update
+    activity = Activity.find_by!(lesson_part_id: params[:lesson_part_id], id: params[:id])
+
+    if activity.update(activity_params)
+      render(json: serialize(activity).to_json, status: :ok)
+    else
+      render(json: { errors: activity.errors.full_messages }, status: :bad_request)
+    end
   end
 
 private
+
+  def activity_params
+    params.require(:activity).permit(:overview, :duration, extra_requirements: [])
+  end
 
   def serialize(lesson_part)
     SimpleAMS::Renderer.new(
