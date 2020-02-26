a57f69649264c29efe7ee486612a23bb6d9e5e96
Peter Yates <peter.yates@graphia.co.uk>
2020-02-26 09:50:13 +0000

    Replace build_stubbed with create to avoid errors
    The errors were caused by adding the missing foreign key between lessons
    and lesson_parts. Also, the files have been renamed to %.slim_spec.rb to
    match the corresponding templates

--- spec/views/teachers/lessons/_lesson_contents.slim_spec.rb
+++ spec/views/teachers/lessons/_lesson_contents.slim_spec.rb
@@ -1,9 +1,9 @@
 require "rails_helper"
 
 RSpec.describe "teachers/lessons/_lesson_contents" do
-  let(:lesson) { FactoryBot.build_stubbed(:lesson) }
-  let(:teacher) { FactoryBot.build_stubbed(:teacher) }
-  let!(:lesson_parts) { create_list(:lesson_part, 2, :with_activities, lesson: lesson) }
+  let(:lesson) { FactoryBot.create(:lesson) }
+  let(:teacher) { FactoryBot.create(:teacher) }
+  let!(:lesson_parts) { FactoryBot.create_list(:lesson_part, 2, :with_activities, lesson: lesson) }
   let(:presenter) { Teachers::LessonContentsPresenter.new(lesson, teacher) }
 
   before :each do
