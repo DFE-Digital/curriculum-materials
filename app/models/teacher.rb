27de27b18bfbf8dadcf65279c894e7458b242550
Richard Lynch <rjlynchdev@gmail.com>
2020-02-28 15:30:38 +0000

    Add Teacher#chosen_activities
    We need a method that give a lesson returns the teachers chosen
    activities.

--- app/models/teacher.rb
+++ app/models/teacher.rb
@@ -5,4 +5,11 @@ class Teacher < ApplicationRecord
 
   has_many :activity_choices, dependent: :destroy
   has_many :activities, through: :activity_choices
+
+  def chosen_activities(lesson)
+    Activity.joins(:activity_choices).where(activity_choices: {
+      teacher: self,
+      lesson_part: lesson.lesson_parts
+    })
+  end
 end
--- spec/models/teacher_spec.rb
+++ spec/models/teacher_spec.rb
@@ -21,4 +21,67 @@ RSpec.describe Teacher, type: :model do
     it { is_expected.to have_many :activity_choices }
     it { is_expected.to have_many(:activities).through :activity_choices }
   end
+
+  context '#chosen_activities' do
+    let! :teacher_1 do
+      create :teacher
+    end
+
+    let! :teacher_2 do
+      create :teacher
+    end
+
+    let! :lesson_1 do
+      create :lesson
+    end
+
+    let! :lesson_2 do
+      create :lesson
+    end
+
+    let! :lesson_1_part do
+      create :lesson_part, lesson: lesson_1
+    end
+
+    let! :lesson_2_part do
+      create :lesson_part, lesson: lesson_2
+    end
+
+    let! :lesson_1_part_activity_1 do
+      create :activity, lesson_part: lesson_1_part
+    end
+
+    let! :lesson_1_part_activity_2 do
+      create :activity, lesson_part: lesson_1_part
+    end
+
+    let! :lesson_2_part_activity_1 do
+      create :activity, lesson_part: lesson_2_part
+    end
+
+    let! :teacher_1_choice do
+      create :activity_choice,
+        teacher: teacher_1,
+        lesson_part: lesson_1_part,
+        activity: lesson_1_part_activity_1
+    end
+
+    let! :teacher_1_choice_for_other_lesson do
+      create :activity_choice,
+        teacher: teacher_1,
+        lesson_part: lesson_2_part,
+        activity: lesson_2_part_activity_1
+    end
+
+    let! :teacher_2_choice do
+      create :activity_choice,
+        teacher: teacher_2,
+        lesson_part: lesson_1_part,
+        activity: lesson_1_part_activity_2
+    end
+
+    subject { teacher_1.chosen_activities lesson_1 }
+
+    it { is_expected.to match_array [teacher_1_choice.activity] }
+  end
 end
