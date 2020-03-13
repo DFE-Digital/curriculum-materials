27db060722370a77dc6a603344b9adb6e84891e0
Peter Yates <peter.yates@graphia.co.uk>
2020-03-13 16:40:44 +0000

    Add basic API token-checking functionality
    The 'master' token can be set using the environment variable 'API_TOKEN'

--- app/controllers/api/base_controller.rb
+++ app/controllers/api/base_controller.rb
@@ -1,5 +1,36 @@
 module Api
+  class InvalidToken < ActiveRecord::RecordNotFound; end
+
   class BaseController < ApplicationController
     protect_from_forgery with: :null_session
+    before_action :ensure_token_exists
+
+    rescue_from InvalidToken, with: :invalid_token
+
+  private
+
+    def ensure_token_exists
+      return true if matches_master_token?
+
+      # check suppliers
+
+      raise InvalidToken, "bad token #{supplied_token}"
+    end
+
+    def supplied_token
+      request.headers['HTTP_API_TOKEN']
+    end
+
+    def invalid_token(e)
+      render(json: { errors: %(Invalid token #{e}) }, status: :unauthorized)
+    end
+
+    def matches_master_token?
+      master_token.present? && supplied_token == master_token
+    end
+
+    def master_token
+      ENV.fetch('API_TOKEN') { nil }
+    end
   end
 end
