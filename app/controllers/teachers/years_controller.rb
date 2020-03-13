cd427ef94bd4a414cd8e642c30dd37b4b41ad466
Peter Yates <peter.yates@graphia.co.uk>
2020-03-13 09:57:21 +0000

    Fix ordering of lessons in a unit card

--- app/controllers/teachers/years_controller.rb
+++ app/controllers/teachers/years_controller.rb
@@ -2,7 +2,12 @@ module Teachers
   class YearsController < BaseController
     def show
       @complete_curriculum_programme = CompleteCurriculumProgramme.find(params[:complete_curriculum_programme_id])
-      @units = @complete_curriculum_programme.units.eager_load(:lessons).at_year(params[:id])
+
+      @units = @complete_curriculum_programme
+        .units
+        .eager_load(:lessons)
+        .merge(Lesson.ordered_by_position)
+        .at_year(params[:id])
     end
   end
 end
